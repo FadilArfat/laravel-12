1d89e8d0d076336bad74959de5396e445a926a93
FadilArfat <fadilitas14@gmail.com>
2025-04-10 11:20:27 +0700

    fix lagi

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,19 @@
 
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::resource('/products', ProductController::class);
+Route::get('/test-db', function () {
+    try {
+        DB::connection()->getPdo();
+        return "DB connection success!";
+    } catch (\Exception $e) {
+        return "DB connection failed: " . $e->getMessage();
+    }
+});
