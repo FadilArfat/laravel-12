5ec8a1d651a4c749bdc1d24e59f10fb84d9a6d9b
FadilArfat <fadilitas14@gmail.com>
2025-04-10 11:22:54 +0700

    lagi

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
 use App\Http\Controllers\ProductController;
+use App\Models\Product;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\DB;
-
+use Illuminate\Support\Facades\Log;
 
 Route::get('/', function () {
     return view('welcome');
@@ -16,5 +17,14 @@ Route::get('/test-db', function () {
         return "DB connection success!";
     } catch (\Exception $e) {
         return "DB connection failed: " . $e->getMessage();
+    }
+});
+Route::get('/test-products', function () {
+    try {
+        $data = Product::all();
+        return response()->json($data);
+    } catch (\Throwable $e) {
+        Log::error('Product query failed', ['error' => $e->getMessage()]);
+        return response()->json(['error' => $e->getMessage()], 500);
     }
 });
